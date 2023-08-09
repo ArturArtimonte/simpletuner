58ba07f2e330e7426a8b7301c850729e8cdd4044
ArturArtimonte <artur.artimonte@gmail.com>
2023-08-09 00:03:05 -0300

    first page of the app, tuner

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,140 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+import 'package:fftea/fftea.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'dart:async';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Simple Tuner',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TunerScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TunerScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TunerScreenState createState() => _TunerScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TunerScreenState extends State<TunerScreen> {
+  FlutterSoundRecorder? _recorder = FlutterSoundRecorder();
+  bool _isRecording = false;
+  bool _mRecorderIsInited = false;
+  String? _recordingPath;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  double _currentFrequency = 0;
+  FFT? fft;
+  List<double> myData = [];
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(milliseconds: 100), (timer) {
+      _processAudioData();
     });
+    _initializeRecorder();
+  }
+
+  Future<void> _initializeRecorder() async {
+    await _recorder!.openRecorder();
+    Directory tempDir = await getTemporaryDirectory();
+    _recordingPath = '${tempDir.path}/recording.pcm';
+    setState(() {
+      _mRecorderIsInited = true;
+    });
+  }
+
+  void _processAudioData() {
+    if (fft == null) return; // Ensure FFT is initialized
+    final freq = fft!.realFft(myData);
+    if (freq != null && freq.isNotEmpty) {
+      double frequency = _calculateFrequency(freq as List<double>);
+      setState(() {
+        _currentFrequency = frequency;
+      });
+    }
+  }
+
+  double _calculateFrequency(List<double> data) {
+    return data.reduce((value, element) => value + element) / data.length;
+  }
+
+  Future<void> _startRecording() async {
+    await _recorder!.startRecorder(
+      toFile: _recordingPath,
+      codec: Codec.pcm16,
+    );
+    setState(() {
+      _isRecording = true;
+    });
+  }
+
+  Future<void> _stopRecording() async {
+    await _recorder!.stopRecorder();
+    setState(() {
+      _isRecording = false;
+    });
+    _processRecording();
+  }
+
+  Future<void> _processRecording() async {
+    myData = await _fetchAudioData();
+    fft = FFT(myData.length); // Initialize FFT here
+    _processAudioData();
+  }
+
+  Future<List<double>> _fetchAudioData() async {
+    final file = File(_recordingPath!);
+    final rawData = await file.readAsBytes();
+    return rawData.map((byte) => byte.toDouble()).toList();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _recorder!.closeRecorder();
+    _recorder = null;
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Guitar Tuner'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+        child: SfRadialGauge(
+          axes: <RadialAxis>[
+            RadialAxis(minimum: 0, maximum: 1000, pointers: <GaugePointer>[
+              NeedlePointer(value: _currentFrequency)
+            ], annotations: <GaugeAnnotation>[
+              GaugeAnnotation(
+                  widget: Text('$_currentFrequency Hz'),
+                  angle: 90,
+                  positionFactor: 0.5)
+            ])
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: _mRecorderIsInited
+          ? FloatingActionButton(
+              onPressed: _isRecording ? _stopRecording : _startRecording,
+              tooltip: _isRecording ? 'Stop Recording' : 'Start Recording',
+              child: Icon(_isRecording ? Icons.stop : Icons.mic),
+            )
+          : null,
     );
   }
 }
